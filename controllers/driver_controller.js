20829e2d9e0570e97243353e10829c1cee40645b
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2023-02-14 14:06:28 +0530

    crud completed

--- controllers/driver_controller.js
+++ controllers/driver_controller.js
@@ -9,10 +9,35 @@ module.exports = {
     const driverProps =  req.body;
     console.log(driverProps)
 
-    Driver.create(driverProps).then(driver => res.send(driver)).catch(next());
+    Driver.create(driverProps).then(driver => res.send(driver)).catch(next);
 
 
 
 
+  },
+  edit(req , res , next){
+
+    const driverId = req.params.id;
+
+    const driverProps =  req.body;
+
+    Driver.findByIdAndUpdate({_id: driverId} , driverProps)
+    .then(()=>{
+      Driver.findById({_id:driverId})
+      
+    })
+    .then(driver => res.send(driver))
+    .catch(next);
+
+  }
+  ,
+  delete(req , res , next){
+    const driverId = req.params.id;
+
+    Driver.findByIdAndRemove({_id:driverId}).then((driver)=>{
+
+      res.send(driver)
+    }).catch(next);
+    
   }
 };
--- test/controllers/drivers_controller_test.js
+++ test/controllers/drivers_controller_test.js
@@ -4,6 +4,7 @@ const request = require("supertest");
 
 const app = require("../../app");
 const mongoose = require("mongoose");
+const driver_controller = require("../../controllers/driver_controller");
 const Driver = mongoose.model("driver");
 
 describe("Driver controller", () => {
@@ -13,12 +14,48 @@ describe("Driver controller", () => {
         .post("/api/drivers")
         .send({ email: "test@test.com" })
         .end((err, response) => {
-          Driver.count().then((newCount) => {            
-            assert(count +1 == newCount);
-             done();
+          Driver.count().then((newCount) => {
+            assert(count + 1 == newCount);
+            done();
           });
-  
-         done();
+
+          done();
+        });
+    });
+  });
+
+  it("PUT to /api/drivers/id edits an existing driver", (done) => {
+    const driver = new Driver({ email: "t@t.com", driving: false });
+    driver.save().then(() => {
+      request(app)
+        .put(`/api/drivers/${driver._id}`)
+        .send({ driving: true })
+        .end(() => {
+          Driver.findOne({ email: "t@t.com" }).then((driver) => {
+            assert(driver.driving === true);
+
+            done();
+          });
+        });
+    });
+  });
+
+  it("DELETE to /api/drivers/id deletes a driver", (done) => {
+ 
+    const driver = new Driver({ email: "test@test.com", driving: false });
+
+    driver.save().then(() => {
+      request(app)
+        .delete(`/api/drivers/${driver._id}`)
+        .end(() => {
+       
+          Driver.findOne({ email: "test@test.com" })
+          .then((driver) => {
+          
+            assert(driver === null);
+
+            done();
+          })
         });
     });
   });
